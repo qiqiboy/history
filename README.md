df597a86da641bdb63c4a7c03cfe12df5c42fba1
qiqiboy <imqiqiboy@gmail.com>
2014-04-24 12:08:59 +0800

    add file

---
+++ README.md
@@ -0,0 +1,42 @@
+myHistory
+======
+
+> javascript历史记录
+
+## 注意
+该组件依赖于我开发另一个storage组件storage.js(https://github.com/qiqiboy/storage)，以及一个JSON.js(为ie6 7增加JSON功能，https://github.com/douglascrockford/JSON-js)。
+添加记录需要指定一个键值用来保证记录唯一性。
+索引越小的数据越新。
+
+## 如何使用
+```javascript
+// 首先在页面中依次引入storage.js, JSON.js(JSON2.js),history.js
+// 实例化一个历史记录引用对象
+var history=new myHistory('myHistory');
+
+// 添加记录
+history.add(key, data);
+
+// 删除记录
+history.remove(id);
+
+// 清空记录
+history.empty();
+
+// 获取索引键值
+history.key(i);
+
+// 获取索引记录数据
+history.index(i);
+
+// 获取键值所对应的数据
+history.get(key);
+
+// 遍历历史数据记录
+// 从0到最大，记录由新到旧，即索引为0的为最新的记录
+history.walk(function(data, key){});
+
+````
+
+## DEMO 
+无
---
+++ history.js
@@ -0,0 +1,77 @@
+(function(ROOT, NS, undefined){
+    
+    function myHistory(name){
+        this.storage=new storage(name);
+        this.checkSize();
+    }
+
+    myHistory.prototype={
+        maxSize:10,//历史记录最大数量
+        length:0,
+        orderArray:[],
+        constructor:myHistory,
+        //获取键值为id的数据记录
+        get:function(id){
+            return JSON.parse(this.storage.get(id));
+        },
+        //增加数据，id作为判断唯一性键值
+        add:function(id,obj){
+            if(!obj.time){
+                obj.time=+new Date;
+            }
+            this.storage.set(id,JSON.stringify(obj));
+            this.checkSize();
+        },
+        //删除键值为id的数据记录
+        remove:function(id){
+            this.storage.remove(id);
+            this.checkSize();
+        },
+        //遍历历史记录
+        walk:function(func){
+            var i=0,j=this.length;
+            for(;i<j;i++){
+                if(false===func(this.get(this.key(i))))
+                    break;
+            }
+        },
+        //清空历史记录
+        empty:function(){
+            this.storage.clear();
+            this.checkSize();
+        },
+        //获取索引i所对应的键值
+        key:function(i){
+            return this.orderArray[i];
+        },
+        //获取索引为i所对应的数据
+        index:function(i){
+            return this.get(this.key(i));
+        },
+        //整理数据记录
+        checkSize:function(){
+            var self=this,
+                i=0,j=this.storage.size();
+            this.orderArray=[];
+            for(;i<j;i++){
+                this.orderArray.push(this.storage.key(i));
+            }
+            this.orderArray.sort(function(k1,k2){
+                var v1=self.get(k1),
+                    v2=self.get(k2);
+                return v1.time<v2.time?1:-1;
+            });
+            this.length=this.orderArray.length;
+            while(this.length>this.maxSize){
+                this.length--;
+                this.remove(this.key(this.length));
+            }
+            this.orderArray.length=this.length;
+        }
+    }
+
+    ROOT[NS]=myHistory;
+
+})(window, 'myHistory');
+    
+	
